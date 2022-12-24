efe8ecce032d6ef6dc61b1587f196cc0393adf0f
harshitsan <harshitbhalla.hb@gmail.com>
2022-12-25 00:17:39 +0530

    add keystone landing page

---
+++ assets/formValidator.js
@@ -0,0 +1,47 @@
+$(document).ready( function () {
+    $( "#contactForm" ).validate( {
+      rules: {
+        contactName: "required",
+        contactEmail: "required",
+        contactMessage: "required",
+      },
+      messages: {
+        contactName: "Let us know who to contact",
+        contactEmail: "Let us know where to respond",
+        contactMsg: "Let us know how we can help"
+      },
+      errorElement: "em",
+				errorPlacement: function ( error, element ) {
+					// Add the `help-block` class to the error element
+					error.addClass( "help-block" );
+					if ( element.prop( "type" ) === "checkbox" ) {
+						error.insertAfter( element.parent( "label" ) );
+					} else {
+						error.insertAfter( element );
+					}
+				},
+				highlight: function ( element, errorClass, validClass ) {
+					$( element ).parents( ".col-sm-5" ).addClass( "has-error" ).removeClass( "has-success" );
+				},
+				unhighlight: function (element, errorClass, validClass) {
+					$( element ).parents( ".col-sm-5" ).addClass( "has-success" ).removeClass( "has-error" );
+				}
+
+
+    })});
+    /*  errorElement: "em",
+      errorPlacement: function ( error, element ) {
+        // Add the `help-block` class to the error element
+        error.addClass( "help-block" );
+        if ( element.prop( "type" ) === "checkbox" ) {
+          error.insertAfter( element.parent( "label" ) );
+        } else {
+          error.insertAfter( element );
+        }
+      },
+      highlight: function ( element, errorClass, validClass ) {
+        $( element ).parents( ".col-sm-5" ).addClass( "has-error" ).removeClass( "has-success" );
+      },
+      unhighlight: function (element, errorClass, validClass) {
+        $( element ).parents( ".col-sm-5" ).addClass( "has-success" ).removeClass( "has-error" );
+      }*/
